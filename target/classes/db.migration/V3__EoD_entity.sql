52549bdfe3ea8e4c5793eacadbf61c4a08e2a599
scandiy <38243615+scandiy@users.noreply.github.com>
2023-12-22 10:39:26 +0200

    Delete target directory

--- target/classes/db.migration/V3__EoD_entity.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE school.endofday (
-                                 symbol VARCHAR(255) NOT NULL,
-                                 date DATE NOT NULL,
-                                 close FLOAT NOT NULL,
-                                 PRIMARY KEY (symbol, date)
-);
